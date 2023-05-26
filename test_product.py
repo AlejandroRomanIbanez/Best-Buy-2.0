b63ba55bf31523782d4f56c2bf205ed2e69c3a27
Entseeker <entseeker1995@gmail.com>
2023-05-26 12:24:52 +0200

    Added new test for product limit and all promotions

--- test_product.py
+++ test_product.py
@@ -1,12 +1,13 @@
 import pytest
-from products import Product
+import products
+import promotions
 
 
 def test_create_normal_prod():
     """
     Test creating a normal product.
     """
-    normal_prod = Product("Iphone14", 1300, 200)
+    normal_prod = products.Product("Iphone14", 1300, 200)
 
 
 def test_create_invalid_prod():
@@ -14,16 +15,16 @@ def test_create_invalid_prod():
     Test creating an invalid product.
     """
     with pytest.raises(ValueError):
-        Product("sadasd", 1300, -200)
-        Product("sadasd", -1300, 200)
-        Product("", 1300, 200)
+        products.Product("sadasd", 1300, -200)
+        products.Product("sadasd", -1300, 200)
+        products.Product("", 1300, 200)
 
 
 def test_prod_becomes_inactive():
     """
     Test if a product becomes inactive when its quantity is set to zero.
     """
-    inactive_product = Product("sadasd", 1300, 5)
+    inactive_product = products.Product("sadasd", 1300, 5)
     inactive_product.set_quantity(0)
     assert not inactive_product.active
 
@@ -32,7 +33,7 @@ def test_buy_modifies_quantity():
     """
     Test if buying a product modifies its quantity.
     """
-    product = Product("Example Product", 10, 20)
+    product = products.Product("Example Product", 10, 20)
     purchase_quantity = 5
     expected_quantity = product.quantity - purchase_quantity
     expected_output = f"Total price: {product.price * purchase_quantity}"
@@ -47,6 +48,34 @@ def test_buy_too_much():
     """
     Test buying too much quantity of a product.
     """
-    product = Product("Example Product", 10, 20)
+    product = products.Product("Example Product", 10, 20)
     with pytest.raises(ValueError):
         product.buy(250)
+
+
+def test_pass_the_limit():
+    """
+    Test buying more than the max limit
+    """
+    product = products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
+    with pytest.raises(ValueError):
+        product.buy(2)
+
+
+def test_all_promotions():
+    """
+    Test that all promotions works as it should be
+    """
+    product = products.Product("Example Product", 10, 5)
+    second_half_price = promotions.SecondHalfPrice("Second Half price!")
+    third_one_free = promotions.ThirdOneFree("Third One Free!")
+    percent_discount = promotions.PercentDiscount("30% off!", percent=30)
+
+    total_price = second_half_price.apply_promotion(product, 4)
+    assert total_price == 20
+    total_price = third_one_free.apply_promotion(product, 6)
+    assert total_price == 40
+    total_price = percent_discount.apply_promotion(product, 3)
+    assert total_price == 21
+
+
